d2d07720517c5f097c51295ca2a431908a210940
lyanbrito <lyanbrito613@gmail.com>
2024-06-29 11:45:06 -0300

    feat:finished-send-email-scriptt

--- src/service/email/script/send-email.js
+++ src/service/email/script/send-email.js
@@ -1,12 +1,22 @@
 emailjs.init({
-    publicKey: ""
+    publicKey: "yre5Wvh_XuuyeDzqJ"
 
-})  
+})
 function handleEmail() {
-    let templateParams = {,
-        name: "Destinatário"
+    let templateParams = {
+        name: "Destinatário",
+        user_email: "lyanbrito613@gmail.com",
         from_name: "Suporte de Domingo",
-
+        message: "Conteúdo da mensagem aqui..."
     }
 
+
+    emailjs.send("service_n0iuc6m", "template_lrujwty", templateParams)
+        .then(function (response) {
+            console.log('sucesso', response.status, response.text)
+            alert('sucesso')
+        }), function(error) {
+            console.error('erro ao enviar o email')
+
+        }
 }
